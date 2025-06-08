6bba9622a8c547f0a421415aa6ea523cc84e6f38
林彥銓 <iandoge2005@gmail.com>
2025-06-09 00:30:13 +0800

    Initial commit

---
+++ src/final_project/Main.java
@@ -0,0 +1,28 @@
+package final_project; // 替換為實際使用的包名
+
+import java.lang.*;
+import javafx.application.Application;
+import javafx.stage.*;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+
+public class Main extends Application {
+
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("start.fxml")); // 載入 FXML 文件
+        stage.setTitle("final");
+        stage.setScene(new Scene(root, 600, 400)); // 設定視窗大小
+        stage.show(); // 顯示視窗
+
+    }
+
+    public static void main(String[] args) {
+        launch(args); // 啟動應用程式
+    }
+}
