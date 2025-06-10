92533c4e00052f002022c011fffe5bfe02adc7d4
林彥銓 <iandoge2005@gmail.com>
2025-06-10 22:42:06 +0800

    嘻嘻

---
+++ src/final_project/ConStart.java
@@ -0,0 +1,12 @@
+package final_project;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+
+import java.util.*;
+
+public class ConStart {
+}
